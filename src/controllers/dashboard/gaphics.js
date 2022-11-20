371bea9d0a95374cb4764e392b98ac932f4fe4b9
Leonel Barreto <99758987+LeonelBarreto@users.noreply.github.com>
2022-11-20 19:42:02 -0300

    Update gaphics.js

--- src/controllers/dashboard/gaphics.js
+++ src/controllers/dashboard/gaphics.js
@@ -2,11 +2,11 @@ const knex = require('../../database/connection');
 
 const allGraphicsDashboard = async (req, res) => {
     try {
-        const averageSalesByMonth = knex.raw(`SELECT AVG(valor), EXTRACT(MONTH FROM data) as month, EXTRACT(YEAR FROM data) as year FROM vendas GROUP BY EXTRACT(MONTH FROM data), EXTRACT (YEAR FROM data)`);
+        const averageSalesByMonth = await knex.raw(`SELECT AVG(valor), EXTRACT(MONTH FROM data) as month, EXTRACT(YEAR FROM data) as year FROM vendas GROUP BY EXTRACT(MONTH FROM data), EXTRACT (YEAR FROM data)`);
 
-        const topSellers = knex.raw(`SELECT SUM(valor), vd.nome FROM vendas v INNER JOIN vendedores vd ON vd.id = v.vendedor_id GROUP BY vd.nome`);
+        const topSellers = await knex.raw(`SELECT SUM(valor), vd.nome FROM vendas v INNER JOIN vendedores vd ON vd.id = v.vendedor_id GROUP BY vd.nome`);
 
-        const allSalesByMonth = knex.raw(`SELECT SUM(valor), EXTRACT(MONTH FROM data) as month, EXTRACT(YEAR FROM data) as year FROM vendas GROUP BY EXTRACT(MONTH FROM data), EXTRACT (YEAR FROM data)`);
+        const allSalesByMonth = await knex.raw(`SELECT SUM(valor), EXTRACT(MONTH FROM data) as month, EXTRACT(YEAR FROM data) as year FROM vendas GROUP BY EXTRACT(MONTH FROM data), EXTRACT (YEAR FROM data)`);
 
         const allGrapichs = {
             averageSalesByMonth,
