628b5e3e48cd0693ec7c1b0f1a164105edbf55c4
Leonel Barreto <99758987+LeonelBarreto@users.noreply.github.com>
2022-11-21 01:54:35 -0300

    Update salesByMonth.js

--- src/controllers/dashboard/salesByMonth.js
+++ src/controllers/dashboard/salesByMonth.js
@@ -2,7 +2,7 @@ const knex = require('../../database/connection');
 
 const salesByMonth = async (req, res) => {
     try {
-        const allSalesByMonth = await knex.raw(`SELECT SUM(valor), EXTRACT(MONTH FROM data) as month, EXTRACT(YEAR FROM data) as year FROM vendas GROUP BY EXTRACT(MONTH FROM data), EXTRACT (YEAR FROM data)`);
+        const allSalesByMonth = await knex.raw(`SELECT SUM(valor), EXTRACT(MONTH FROM data) as month, EXTRACT(YEAR FROM data) as year FROM vendas GROUP BY EXTRACT(MONTH FROM data), EXTRACT (YEAR FROM data) ORDER BY month ASC`);
 
         const salesMonth = allSalesByMonth.rows
 
